ef0a8506843c11e0fe951a2fe632e742248aea86
Alix Hennion <hennion.pierre.alix@hotmail.fr>
2023-07-19 09:26:53 +0200

    modal modification

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,28 +1,43 @@
-
 // Sélection des éléments du DOM
-const modal = document.getElementById("modal");
-const btnOpenModal = document.getElementById("btnOpenModal");
-const closeModal = document.getElementsByClassName("close")[0];
+let modal = document.querySelector("#modal1");
+const btnOpenModal = document.querySelector(".btnOpenModal");
+const btncloseModal = document.querySelector(".close")
+const modalContainer = document.querySelector(".modal-container");
+const galleryImages = document.querySelectorAll("#portfolio .gallery  img");
 
 // Fonction pour ouvrir la modale
 function openModal() {
   modal.style.display = "flex";
+  modalContainer.innerHTML = ""; // Effacer le contenu précédent de la modale
+
+  // Ajouter les images de la galerie à la modale
+  galleryImages.forEach(function (image) {
+    const img = document.createElement("img");
+    img.src = image.src;
+    img.alt = image.alt;
+    modalContainer.appendChild(img);
+  });
+  
+
 }
+// Événement clic sur le bouton pour ouvrir la modale
+btnOpenModal.addEventListener("click", openModal);
 
 // Fonction pour fermer la modale
 function closeModal() {
   modal.style.display = "none";
 }
 
-// Événement clic sur le bouton pour ouvrir la modale
-btnOpenModal.addEventListener("click", openModal);
 
 // Événement clic sur la croix pour fermer la modale
-closeModal.addEventListener("click", closeModal);
-
+btncloseModal.addEventListener("click", closeModal);
 // Événement clic en dehors de la modale pour la fermer
 window.addEventListener("click", function (event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     closeModal();
   }
 });
+
+
+
+
